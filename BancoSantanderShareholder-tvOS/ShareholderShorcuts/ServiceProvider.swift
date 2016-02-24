feb52e2513b5c5514b73bc79b6b7fcf4e832ac50
Michel Barbou Salvador <mibarbou@gmail.com>
2016-02-24 12:26:59 +0100

    Con las imagenes de los top shelfs con las capturas de las secciones.

--- BancoSantanderShareholder-tvOS/ShareholderShorcuts/ServiceProvider.swift
+++ BancoSantanderShareholder-tvOS/ShareholderShorcuts/ServiceProvider.swift
@@ -25,11 +25,11 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
     var topShelfItems: [TVContentItem] {
         // Create an array of TVContentItems.
         
-//        let item1 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item1", container: nil)!)
-//        let item2 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item2", container: nil)!)
-//        let item3 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item3", container: nil)!)
-//        
-//        return [item1!,item2!,item3!]
+        //        let item1 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item1", container: nil)!)
+        //        let item2 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item2", container: nil)!)
+        //        let item3 = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "myapp.item3", container: nil)!)
+        //
+        //        return [item1!,item2!,item3!]
         
         // Create an array of TVContentItems.
         let wrapperID = TVContentIdentifier(identifier: "shelf-wrapper", container: nil)!
@@ -41,48 +41,45 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
             let identifier = TVContentIdentifier(identifier: "VOD", container: wrapperID)!
             let contentItem = TVContentItem(contentIdentifier: identifier )!
             
-            if let url = NSURL(string: "http://www.brianjcoleman.com/code/images/feature-\(i).jpg")
-            {
-            
-                switch i {
-                case 0:
-                    contentItem.imageURL = url
-                    contentItem.imageShape = .HDTV
-                    contentItem.title = "Vídeos"
-                    contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
-                    contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
-                case 1:
-                    contentItem.imageURL = url
-                    contentItem.imageShape = .HDTV
-                    contentItem.title = "Documentos"
-                    contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
-                    contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
-                case 2:
-                    contentItem.imageURL = url
-                    contentItem.imageShape = .HDTV
-                    contentItem.title = "Accionistas"
-                    contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
-                    contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
-                case 3:
-                    contentItem.imageURL = url
-                    contentItem.imageShape = .HDTV
-                    contentItem.title = "Cotizaciones"
-                    contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
-                    contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
-                default:
-                    break
-                    
-                }
+            switch i {
+            case 0:
+                contentItem.imageURL = NSBundle.mainBundle().URLForResource("videos", withExtension: "png")
+                contentItem.imageShape = .HDTV
+                contentItem.title = "Vídeos"
+                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
+                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
+            case 1:
+                contentItem.imageURL = NSBundle.mainBundle().URLForResource("documentos", withExtension: "png")
+                contentItem.imageShape = .HDTV
+                contentItem.title = "Documentos"
+                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
+                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
+            case 2:
+                contentItem.imageURL = NSBundle.mainBundle().URLForResource("TopShelf", withExtension: "png")
+                contentItem.imageShape = .HDTV
+                contentItem.title = "Accionistas"
+                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
+                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
+            case 3:
+                contentItem.imageURL = NSBundle.mainBundle().URLForResource("cotizaciones", withExtension: "png")
+                contentItem.imageShape = .HDTV
+                contentItem.title = "Cotizaciones"
+                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
+                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
+            default:
+                break
+                
             }
             
-//            if let url = NSURL(string: "http://www.brianjcoleman.com/code/images/feature-\(i).jpg")
-//            {
-//                contentItem.imageURL = url
-//                contentItem.imageShape = .HDTV
-//                contentItem.title = "Movie Title"
-//                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
-//                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
-//            }
+            
+            //            if let url = NSURL(string: "http://www.brianjcoleman.com/code/images/feature-\(i).jpg")
+            //            {
+            //                contentItem.imageURL = url
+            //                contentItem.imageShape = .HDTV
+            //                contentItem.title = "Movie Title"
+            //                contentItem.displayURL = NSURL(string: "SantanderShareholder://section/\(i)");
+            //                contentItem.playURL = NSURL(string: "VideoApp://video/\(i)");
+            //            }
             ContentItems.append(contentItem)
         }
         
@@ -92,6 +89,7 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         
         return [wrapperItem]
     }
+
 
 }
 
